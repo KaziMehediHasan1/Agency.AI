e91030042bcb9c8d46b256989b417cd723f9b172
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-09-05 10:55:05 +0600

    completed full responsive navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 
 const App = () => {
+  const [theme, setTheme] = useState("light");
   return (
-    <div>
-      <Navbar />
+    <div className="dark:bg-black relative">
+      <Navbar theme={theme} setTheme={setTheme} />
     </div>
   );
 };
