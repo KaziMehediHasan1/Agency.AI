e0192ae78545bef20a481b657877143d1450f1f0
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-09-07 19:35:59 +0600

    add contactus successfully

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import TrustedCompa from "./components/TrustedCompa";
 import Services from "./components/Services";
 import OurWork from "./components/OurWork";
 import Teams from "./components/Teams";
+import ContactUs from "./components/ContactUs";
+import { Toaster } from "react-hot-toast";
 type Theme = "light" | "dark";
 const App = () => {
   const [theme, setTheme] = useState<Theme>(
@@ -12,12 +14,14 @@ const App = () => {
   );
   return (
     <div className="dark:bg-black relative">
+      <Toaster position="top-center" />
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
       <TrustedCompa />
       <Services />
       <OurWork />
       <Teams />
+      <ContactUs />
     </div>
   );
 };
